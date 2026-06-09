4e373382e382d7f1c0e2cbcef52aca22a8d4aafc
abdugafforovjavohir69-arch <abdugafforovjavohir69>
2026-06-09 12:38:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let open = document.getElementById("open")
+let modal = document.getElementById("modal")
+let yopish = document.getElementById("closse")
+open.addEventListener("click" , function(){
+modal.style.display="block";
+})
+closse.addEventListener("click", function(){
+    modal.style.display="none"
+})
